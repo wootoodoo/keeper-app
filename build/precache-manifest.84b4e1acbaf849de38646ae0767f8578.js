f7933ad0ea4f7a5b2cecdaa792945a1ece2fb5d0
Capt JonLee <jon.lee.choon.yang@gmail.com>
2021-02-04 12:02:36 +0800

    first commit

---
+++ build/precache-manifest.84b4e1acbaf849de38646ae0767f8578.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f87fe16c5933fa005dde122fdb197a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a361041e1995ea307996",
+    "url": "/static/js/2.efb365b0.chunk.js"
+  },
+  {
+    "revision": "c93ba399db7a6523fec3",
+    "url": "/static/js/main.80f599f8.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
